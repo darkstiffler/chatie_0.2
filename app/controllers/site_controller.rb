64d61b3bbeef5e08d3270295a8eab3782fcf86f5
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-22 00:31:15 -0700

    method errors

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,14 +1,7 @@
 class SiteController < ApplicationController
 
   def index
-  	@messages = Message.all 
+  	@messages = Message.all
   end
 
-  def create
-  	@message = Message.create(messge_params)
-  end
-
-  def message_params
-  	params.require(:message).permit(:body)
-  end
 end
