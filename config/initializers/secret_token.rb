1719752933c1f527fb08df5bd8eacb28c75a9b93
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-21 23:18:12 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chatie02::Application.config.secret_key_base = 'f4c9ea76556b3e73a9015ad4e7b1a8b70dab045d1754963b00b49ca6d5410338f094409af6ff6c5f0eea8afaee7ca53da8bf9fd2145dff3af611a926aca08008'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chatie02::Application.config.session_store :cookie_store, key: '_chatie_0_2_session'
