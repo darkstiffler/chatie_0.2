aae3bad44cdc645c125341963580445e2a013ffa
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-21 23:40:34 -0700

    add working msg

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,14 @@
 class SiteController < ApplicationController
-  def load
+
+  def index
+  	@messages = Message.all 
+  end
+
+  def create
+  	@message = Message.create(messge_params)
+  end
+
+  def message_params
+  	params.require(:message).permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Chatie02::Application.routes.draw do
-  get "site/load"
+  get "site/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'site#load'
+   root 'site#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
