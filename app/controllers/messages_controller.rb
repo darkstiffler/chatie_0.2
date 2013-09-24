793f293db5281ffaece7338922e9b8b6ba496f0d
Ethan Stiffelman <eg.stiffelman@gmail.com>
2013-09-23 18:42:39 -0700

    spaggetti...

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,12 @@
+class MessagesController < ApplicationController
+
+	def index
+		@messages = Message.all
+	end
+
+	def new
+	end
+
+	def create
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130922062034) do
+ActiveRecord::Schema.define(version: 20130924013810) do
 
   create_table "messages", force: true do |t|
     t.string   "body"
